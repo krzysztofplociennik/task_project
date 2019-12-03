2c8b364c802e95c4cf6080a5e72fa3a9c0951c46
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2019-12-03 10:51:05 +0100

    29.1\

--- src/main/java/com/crud/tasks/domain/TrelloCard.java
+++ src/main/java/com/crud/tasks/domain/TrelloCard.java
@@ -2,12 +2,10 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
-public class TrelloCardDto {
+@AllArgsConstructor
+public class TrelloCard {
     private String name;
     private String description;
     private String pos;
